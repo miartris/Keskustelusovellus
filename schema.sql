97744d4fc6e41e8d0ce147aa6144c8969ae8efba
Mikko Ristimäki <mikko.a.ristimaki@gmail.com>
2023-03-04 02:09:19 +0200

    update database schema to accommodate further features

--- schema.sql
+++ schema.sql
@@ -1,7 +1,9 @@
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     username TEXT UNIQUE,
-    password TEXT
+    password TEXT,
+    is_admin BOOLEAN, 
+    description TEXT
     );
 
 CREATE TABLE topics (
@@ -21,5 +23,32 @@ CREATE TABLE posts (
     post_id SERIAL PRIMARY KEY,
     creator_id INTEGER REFERENCES users(user_id),
     thread_id INTEGER REFERENCES threads(thread_id),
-    content TEXT CHECK (length(content) <= 500)
+    content TEXT CHECK (length(content) <= 500),
+    date_created TIMESTAMP,
+    upvotes INTEGER
     );
+
+CREATE TABLE private_messages (
+    message_id SERIAL PRIMARY KEY,
+    sender_id INTEGER REFERENCES users(user_id),
+    receiver_id INTEGER REFERENCES users(user_id),
+    content TEXT CHECK (length(content) <= 200),
+    date_sent TIMESTAMP
+);
+
+CREATE TABLE images (
+    image_id SERIAL PRIMARY KEY, 
+    data BYTEA
+);
+
+CREATE TABLE user_images (
+    user_id INTEGER REFERENCES users(user_id),
+    image_id INTEGER REFERENCES images(image_id),
+    PRIMARY KEY (user_id, image_id)
+);
+
+CREATE TABLE post_images (
+    post_id INTEGER REFERENCES posts(post_id),
+    image_id INTEGER REFERENCES images(image_id),
+    PRIMARY KEY (post_id, image_id)
+);
